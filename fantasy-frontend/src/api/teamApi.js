dc5ab0b93b49809d549c44db3dffda46ccc2c768
Tank1180 <martinhd53@gmail.com>
2026-08-18 18:15:36 -0600

    Delete fantasy-frontend directory

--- fantasy-frontend/src/api/teamApi.js
+++
@@ -1,8 +0,0 @@
-import axios from "axios";
-
-const API = "http://127.0.0.1:8000";
-
-export const getTeams = async (leagueId) => {
-  const res = await axios.get(`${API}/teams/league/${leagueId}`);
-  return res.data;
-};
